05bbf40b971298318c2517227d511f95785b966c
Stepan Miakchilo <stepan.miakchilo@gmail.com>
2018-04-05 23:47:22 +0300

    lesson 2 'a', 'b' and 'c' levels

--- src/by/it/miakchilo/lesson02/TaskC1.java
+++ src/by/it/miakchilo/lesson02/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.miakchilo.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -11,7 +13,15 @@ Sum = 26
 */
 class TaskC1 {
 
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i1,i2;
+        System.out.print("Type first number, then press [Enter]: ");
+        i1 = sc.nextInt();
+        System.out.print("Type second number, then press [Enter]: ");
+        i2 = sc.nextInt();
+        System.out.printf("Sum = %d%n",i1+i2);
+    }
 
 
 
--- src/by/it/miakchilo/lesson02/TaskC2.java
+++ src/by/it/miakchilo/lesson02/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.miakchilo.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и 4 раза выводит их сумму с обозначением системы счисления на экран в
 ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
@@ -14,6 +16,20 @@ HEX:22+1a=3c
 OCT:42+32=74
 */
 class TaskC2 {
+    public static void main(String[] args) {
+        int i1,i2,sum;
+        Scanner sc;
 
+        sc = new Scanner(System.in);
+        System.out.print("Type first number, then press [Enter]: ");
+        i1 = sc.nextInt();
+        System.out.print("Type second number, then press [Enter]: ");
+        i2 = sc.nextInt();
+        sum = i1+i2;
+        System.out.printf("DEC:%d+%d=%d%n", i1, i2, sum);
+        System.out.printf("BIN:%s+%s=%s%n", Integer.toBinaryString(i1), Integer.toBinaryString(i2), Integer.toBinaryString(sum));
+        System.out.printf("HEX:%s+%s=%s%n", Integer.toHexString(i1), Integer.toHexString(i2), Integer.toHexString(sum));
+        System.out.printf("OCT:%s+%s=%s%n", Integer.toOctalString(i1), Integer.toOctalString(i2), Integer.toOctalString(sum));
+    }
 
 }
