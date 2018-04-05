57de6f39ad0a07242e30018e53a08392bb7733b3
Khmelov <akhmelev@mail.ru>
2018-04-05 21:19:54 +0300

    lesson2 A

--- src/by/it/demologin/lesson02/TaskB1.java
+++ src/by/it/demologin/lesson02/TaskB1.java
@@ -16,13 +16,5 @@ Scanner sc=new Scanner(System.in);
 
  */
 
-import java.util.Scanner;
-
 class TaskB1 {
-
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int i=sc.nextInt();
-        System.out.println(i*i);
-    }
 }
