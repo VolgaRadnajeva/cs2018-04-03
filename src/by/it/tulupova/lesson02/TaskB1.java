da34c0dc290e66e806a9db766f5f20a3826bee87
tulupovakv <anyokamael@gmail.com>
2018-04-06 23:52:43 +0300

    cs2018-04-03

--- src/by/it/tulupova/lesson02/TaskB1.java
+++ src/by/it/tulupova/lesson02/TaskB1.java
@@ -16,5 +16,15 @@ Scanner sc=new Scanner(System.in);
 
  */
 
+import java.util.Scanner;
+
 class TaskB1 {
+    public static void main(String[] args) {
+        System.out.print("Введите любое целое число: ");
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int result = a * a;
+        System.out.println("Квадрат числа равен:" + result);
+    }
 }
+
