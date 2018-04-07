77745dcfb099c6e5ee153e317f52912ba88c9965
miakchilo <stepan.miakchilo@gmail.com>
2018-04-07 14:49:45 +0300

    lesson 2 'a', 'b' and 'c' levels

--- src/by/it/miakchilo/lesson02/TaskA3.java
+++ src/by/it/miakchilo/lesson02/TaskA3.java
@@ -9,7 +9,7 @@ class TaskA3 {
     public static void main(String[] args) {
         int i = 3;
         int j = 4;
-        int k = i*i + j*j;
+        int k = i * i + j * j;
         // System.out.println(i + "*" + i + "+" + j + "*" + j + "=" + k);   <-- простой способ, проверено
         System.out.printf("%d*%d+%d*%d=%d", i, i, j, j, k);             //  <-- более элегантный и читабельный способ, мне кажется
     }
