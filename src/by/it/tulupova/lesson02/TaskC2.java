74b067add3358d23899b350c561a2219ba3b434f
tulupovakv <anyokamael@gmail.com>
2018-04-07 11:07:27 +0300

    lesson02

--- src/by/it/tulupova/lesson02/TaskC2.java
+++ src/by/it/tulupova/lesson02/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.tulupova.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и 4 раза выводит их сумму с обозначением системы счисления на экран в
 ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
@@ -13,7 +15,18 @@ BIN:100010+11010=111100
 HEX:22+1a=3c
 OCT:42+32=74
 */
+import java.util.Scanner;
+
 class TaskC2 {
-
-
+    public static void main(String[] args) {
+        System.out.print("Введите два числа: ");
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = a + b;
+        System.out.println("DEC:" + a + "+" + b + "=" + result);
+        System.out.println("BIN:" + Integer.toBinaryString(a) + "+" + Integer.toBinaryString(b) + "=" + Integer.toBinaryString(result));
+        System.out.println("HEX:" + Integer.toHexString(a) + "+" + Integer.toHexString(b) + "=" + Integer.toHexString(result));
+        System.out.println("OCT:" + Integer.toOctalString(a) + "+" + Integer.toOctalString(b) + "=" + Integer.toOctalString(result));
+    }
 }
