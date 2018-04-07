2ad560859d576970c8663300873389f01d23e205
tulupova <anyokamael@gmail.com>
2018-04-07 15:19:37 +0300

    lesson3 A1 A2

--- src/by/it/tulupova/lesson03/TaskA2.java
+++ src/by/it/tulupova/lesson03/TaskA2.java
@@ -21,5 +21,11 @@ Lesson 03. Task A2. Стихотворение
 Для автоматической проверки откройте и запустите класс Testing
 */
 class TaskA2 {
-
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("df");
+        System.out.println("sd");
+        System.out.println("sd");
+        System.out.println("shgh");
+    }
 }
