78f4a3352d56229c64174a58770cd21fd413b66a
radnajeva <olga.radnayeva@gmail.com>
2018-04-08 19:13:46 +0300

    Lesson3 A1-C2

--- src/by/it/radnajeva/lesson03/TaskC2.java
+++ src/by/it/radnajeva/lesson03/TaskC2.java
@@ -1,4 +1,7 @@
 package by.it.radnajeva.lesson03;
+
+import java.util.Scanner;
+
 /*
     Сумма цифр четырехзначного числа
 
@@ -19,13 +22,37 @@ package by.it.radnajeva.lesson03;
     4. Метод sumDigitsInNumber не должен ничего выводить на экран.
     5. Метод sumDigitsInNumber должен правильно возвращать сумму всех цифр в числе number.
 */
+
 public class TaskC2 {
+    public static int sumDigitsInNumber(int number) {
+        int digit;
+        int sumDigitsInNumber = 0;
+        digit = number % 10;
+        sumDigitsInNumber = sumDigitsInNumber + digit;
+        number = number / 10;
+        digit = number % 10;
+        sumDigitsInNumber = sumDigitsInNumber + digit;
+        number = number / 10;
+        digit = number % 10;
+        sumDigitsInNumber = sumDigitsInNumber + digit;
+        number = number / 10;
+        sumDigitsInNumber = sumDigitsInNumber + number;
+        return sumDigitsInNumber;
+
+    }
+
+/*    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int number=in.nextInt();
+        int result=sumDigitsInNumber(number);
+        System.out.println(result);
+
+    }
+*/
 
 
-
-
-//    public static void main(String[] args) {
-//        System.out.println(sumDigitsInNumber(5467));
-//    }
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(5467));
+    }
 
 }
