bf9fa13004060f62ec2e37b6b5e9d9666257ec5e
tulupova <anyokamael@gmail.com>
2018-04-10 18:38:23 +0300

    lesson3 B1

--- src/by/it/tulupova/lesson03/TaskB1.java
+++ src/by/it/tulupova/lesson03/TaskB1.java
@@ -26,9 +26,9 @@ float f - присваивание в десятичном формате
 strictfp class TaskB1 {
     public static  void main(String[] args) {
         double d = 111.111;
-        byte b = 111;
-        long o = 111L;
-        int h = 111;
+        byte b = 0b111;
+        long o = 0111L;
+        int h = 0x111;
         float f = 111.111F;
         System.out.println(d + b + o + h + f);
         System.out.println(d + " " + b + " " + o + " " + h + " " + f);
