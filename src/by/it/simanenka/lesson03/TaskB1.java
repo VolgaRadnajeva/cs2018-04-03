914a1b440c0924d4d59cbf4175590b9b7d39491e
simanenka <7361856@gmail.com>
2018-04-10 21:16:09 +0300

    lesson3

--- src/by/it/simanenka/lesson03/TaskB1.java
+++ src/by/it/simanenka/lesson03/TaskB1.java
@@ -27,12 +27,10 @@ strictfp class TaskB1 {
     public static  void main(String[] args) {
 
         double d = 111.111;
-
-        byte b;
-        b = 0b1101111;
-        long o = (long) 0157;
-        int h = 0x6f;
-        float f = (float) 111.111;
+        byte b = 0b111;
+        long o = 0111;
+        int h = 0x111;
+        float f = 111.111F;
         System.out.println(d + b + o + h + f);
         System.out.println(d + " " + b + " " + o + " " + h + " " + f);
     }
--- src/by/it/simanenka/lesson03/TaskC2.java
+++ src/by/it/simanenka/lesson03/TaskC2.java
@@ -21,23 +21,7 @@ package by.it.simanenka.lesson03;
 */
 public class TaskC2 {
     public static void main(String[] args) {
-        System.out.println(sumDigitsInNumber(5467));
-    }
-    public static int sumDigitsInNumber(int number) {
-        number = 5467;
-        int sum = 0;
-        int currentDigit;
-        currentDigit = number % 10;
-        sum = sum + currentDigit;
-        number = number / 10;
-        currentDigit = number % 10;
-        sum = sum + currentDigit;
-        number = number / 10;
-        currentDigit = number % 10;
-        sum = sum + currentDigit;
-        number = number / 10;
-        sum = sum + number;
-        return sum;
+
     }
 
 
