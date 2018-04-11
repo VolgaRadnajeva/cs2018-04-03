c43a2e8557f8b2277243e3e1d195e3dc1168bcab
radnajeva <olga.radnayeva@gmail.com>
2018-04-11 23:27:58 +0300

    Lesson4 B3-C2

--- src/by/it/radnajeva/lesson04/TaskC2.java
+++ src/by/it/radnajeva/lesson04/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.radnajeva.lesson04;
 
+import java.util.Scanner;
+
 /*
 
 На входе вводится число и месяц 2018 года.
@@ -27,5 +29,30 @@ package by.it.radnajeva.lesson04;
 
  */
 public class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        int month = sc.nextInt();
+        int i1 = 31;
+        int i2 = 30;
+
+
+        if (month == 1)
+            System.out.println(day);
+        else if (month == 2)
+            System.out.println(i1 + day);
+        else if (month >= 3 && month <= 7) {
+            if ((month % 2) == 0)
+                System.out.println(i1 * (month / 2) + i2 * ((month / 2)-1) - 2 + day);
+            else
+                System.out.println(i1 * (month / 2) + i2 * (month / 2) - 2 + day);
+        } else if (month >= 8 && month <= 12) {
+            if ((month % 2) == 0)
+                System.out.println(i1 * (month / 2) + i2 * (month / 2 - 1) - 2 + day);
+            else
+                System.out.println(i1 * (month / 2 + 1) + i2 * ((month / 2)-1) - 2 + day);
+        }
+
+    }
 
 }
