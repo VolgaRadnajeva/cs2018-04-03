436ca48803e7909eeb5d545fb96c704f063ffa77
Klimenko <tehnomall.by@gmail.com>
2018-04-12 15:17:20 +0300

    Lesson 4 A1-C3

--- src/by/it/Klimenko_Vadim/lesson04/TaskC1.java
+++ src/by/it/Klimenko_Vadim/lesson04/TaskC1.java
@@ -38,7 +38,68 @@ package by.it.Klimenko_Vadim.lesson04;
 */
 
 
+import java.util.Scanner;
 public class TaskC1 {
-
+    public static void main(String[] args) {
+        System.out.println("Какую вы хотите зарплату в $$$? ");
+        Scanner scanner = new Scanner(System.in);
+        int salary = scanner.nextInt();
+        if (salary >= 300 && salary <= 3000) {
+            for (int month = 0; month <= 14; month++) {
+                String month_name;
+                double month_salary;
+                switch (month) {
+                    case 1:
+                        month_name = "январь";
+                        break;
+                    case 2:
+                        month_name = "февраль";
+                        break;
+                    case 3:
+                        month_name = "март";
+                        break;
+                    case 4:
+                        month_name = "апрель";
+                        break;
+                    case 5:
+                        month_name = "май";
+                        break;
+                    case 6:
+                        month_name = "июнь";
+                        break;
+                    case 7:
+                        month_name = "июль";
+                        break;
+                    case 8:
+                        month_name = "август";
+                        break;
+                    case 9:
+                        month_name = "сентябрь";
+                        break;
+                    case 10:
+                        month_name = "октябрь";
+                        break;
+                    case 11:
+                        month_name = "ноябрь";
+                        break;
+                    case 12:
+                        month_name = "декабрь";
+                        break;
+                    default:
+                        month_name = "месяц " + month;
+                }
+                month_salary = salary;
+                if (month==0 || month>12) {
+                    month_salary=0.0;
+                } else if (month<6 || month>8) {
+                    month_salary*=1.5;
+                }
+                System.out.println("За " + month_name + " начислено $" + month_salary);
+                if (month_salary==666.0) break;
+            }
+        } else {
+            System.out.println("Мы вам перезвоним!");
+        }
+    }
 
 }
--- src/by/it/Klimenko_Vadim/lesson04/TaskC2.java
+++ src/by/it/Klimenko_Vadim/lesson04/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.Klimenko_Vadim.lesson04;
 
+import java.util.Scanner;
+
 /*
 
 На входе вводится число и месяц 2018 года.
@@ -27,5 +29,34 @@ package by.it.Klimenko_Vadim.lesson04;
 
  */
 public class TaskC2 {
+    public static void main(String[] args) {
+        System.out.println("Введите число и месяц 2018 года через пробел, нажмите [Enter]: ");
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        int month= sc.nextInt();
+        int total_day=0;
+        if (day <= 0) {
+            day = 1;
+        } else if (day>31)
+            day=1;
+        if (month<=0) month=1;
+        else if (month>12) month=12;
+        int i=1;
+        if (month==1){
+            total_day=0;
+        }
+        else {
+            while (i<month){
+                if (i==1||i==3||i==5 ||i==7 ||i==8 ||i==10)
+                    total_day+=31;
+                else if (i==2)
+                    total_day+=28;
+                            else if (i==4||i==6||i==9||i==11)
+                                total_day+=30;
+             i++;
+            }
+        }
 
+        System.out.println(total_day+day);
+    }
 }
