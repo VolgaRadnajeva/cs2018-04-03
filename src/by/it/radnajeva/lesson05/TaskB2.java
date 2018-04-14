64e412cc32e1635396469f2c8486356eedeb33f0
VolgaRadnajeva <vradnajeva@gmail.com>
2018-04-14 10:13:07 +0300

    Lesson 5 A1-C1

--- src/by/it/radnajeva/lesson05/TaskB2.java
+++ src/by/it/radnajeva/lesson05/TaskB2.java
@@ -20,6 +20,27 @@ b=[11, 22, 33, 44, 55, 66, 77, 88, 99, 0]
 
 */
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TaskB2 {
+    public static void main(String[] args) {
+        int m[] = new int[20];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < m.length; i++) {
+            int value = sc.nextInt();
+            m[i] = value;
+        }
+        int a[] = new int[10];
+        int b[] = new int[10];
+        for (int i = 0; i < a.length; i++)
+            a[i] = m[i];
+        for (int i = 0; i < b.length; i++)
+            b[i] = m[m.length / 2 + i];
+        System.out.println("a=" + Arrays.toString(a));
+        System.out.println("b=" + Arrays.toString(b));
+
+
+    }
 
 }
