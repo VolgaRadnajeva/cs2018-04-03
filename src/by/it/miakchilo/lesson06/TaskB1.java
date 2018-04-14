b5222a94e670dbed48aaaf2b2ef4671ec5f6e7cc
miakchilo <stepan.miakchilo@gmail.com>
2018-04-14 15:22:50 +0300

    everything done

--- src/by/it/miakchilo/lesson06/TaskB1.java
+++ src/by/it/miakchilo/lesson06/TaskB1.java
@@ -32,7 +32,7 @@ public class TaskB1 {
                 new Dog("Жучка",2),
                 new Dog("Бобик",3),
                 new Dog("Барбос",4),
-                new Dog("Полкан",5),
+                new Dog("Полкан",5)
         };
         DogHelper.printAllNames(dogs);
         System.out.println(DogHelper.averageAge(dogs));
